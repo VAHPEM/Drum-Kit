0963ab8de09aa75876e6d1e2f74b423768c8686b
VAHPEM <ve0912084386@gmail.com>
2024-09-29 11:11:20 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+for (var i=0; i<document.querySelectorAll(".drum").length; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        makeSounds(this.innerHTML);
+        effect(this.innerHTML);
+        })
+};
+
+document.addEventListener("keydown", function(event) {
+    makeSounds(event.key);
+    effect(event.key);
+});
+
+function makeSounds(key) {
+    switch (key) {
+        case "w":
+            var audio0 = new Audio("./sounds/crash.mp3")
+            audio0.play()
+        break;
+        case "a":
+            var audio1 = new Audio("./sounds/kick-bass.mp3")
+            audio1.play()
+        break;
+        case "s":
+            var audio2 = new Audio("./sounds/snare.mp3")
+            audio2.play()
+        break;
+        case "d":
+            var audio3 = new Audio("./sounds/tom-1.mp3")
+            audio3.play()
+        break;
+        case "j":
+            var audio4 = new Audio("./sounds/tom-2.mp3")
+            audio4.play()
+        break;
+        case "k":
+            var audio5 = new Audio("./sounds/tom-3.mp3")
+            audio5.play()
+        break;
+        case "l":
+            var audio6 = new Audio("./sounds/tom-4.mp3")
+            audio6.play()
+        break;
+        default: console.log(this.innerHTML)
+    }
+}
+
+function effect(key) {
+    document.querySelector("." + key).classList.add("pressed")
+    setTimeout(function () {
+        document.querySelector("." + key).classList.remove("pressed")
+    }, 100)
+}
